182037e281190b27da518c1466654e037fd862d9
Manuel-Alarcon360 <manuelfernandoalarconr@gmail.com>
2026-01-19 18:29:54 -0500

    cambio tenmporal del endpoint

--- src/app/modules/content/clientes/service/clientes.endpoint.ts
+++ src/app/modules/content/clientes/service/clientes.endpoint.ts
@@ -1,8 +1,12 @@
 const controller = 'integracion/';
 const controllerPacientes = 'paciente/';
+const BASE_URL_TEMP = 'https://25951a85fe9b.ngrok-free.app/api/';
+
 export enum ClienteEndpoint {
-    consultarClientes = controller + 'obtener_clientes_externos/',
-    crearCliente = controller + 'crear_cliente/',
+    // consultarClientes = controller + 'obtener_clientes_externos/',
+    // crearCliente = controller + 'crear_cliente/',
+    consultarClientes = BASE_URL_TEMP + 'integracion/obtener_clientes_externos/',
+    crearCliente =  BASE_URL_TEMP + 'integracion/crear_cliente/',
     getPaciente = controllerPacientes + 'get_pacientes/',
     searchCliente =  controller + "buscar_cliente/",
 
